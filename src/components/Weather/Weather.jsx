f6a69ab1b0bb656d841e13334b752287be5a1ed3
sergio rodas <rodascruzsergio@gmail.com>
2022-06-01 17:10:33 -0400

    added in the folder components at folder Weather and files Weather jsx, stories and test the components

---
+++ src/components/Weather/Weather.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Typography from '@material-ui/core/Typography'
+import { WiCloud } from 'react-icons/wi'
+
+const Weather = ({ temperature }) => {
+  return (
+    <div>
+      <WiCloud></WiCloud>
+      <Typography display='inline' variant='h2'>{temperature}</Typography>
+    </div>
+  )
+}
+
+Weather.propTypes = {
+  temperature: PropTypes.number.isRequired
+}
+
+export default Weather
---
+++ src/components/Weather/Weather.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Weather from './Weather'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+
+test('Weather render', async () => {
+  // AAA Arrange Act Assert
+  const { findByRole } = render(<Weather temperature={10} />)
+
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  const temp = await findByRole('heading')
+
+  expect(temp).toHaveTextContent('10')
+})
