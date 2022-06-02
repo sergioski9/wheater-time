e1d4b4c64363a0f27d2b7cc98c2a05b4b75b9f82
sergio rodas <rodascruzsergio@gmail.com>
2022-06-01 22:37:53 -0400

    added component weather and files index component stories and test

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,19 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Typography from '@material-ui/core/Typography'
-import { WiCloud } from 'react-icons/wi'
+import { WiCloud, WiDayCloudy,
+  WiDayFog,
+  WiDaySunny,
+  WiRain } from 'react-icons/wi'
+import { IconContext } from 'react-icons'
 
-const Weather = ({ temperature }) => {
+
+const stateByName = {
+  cloud: WiCloud,
+  cloudy: WiDayCloudy,
+  fog: WiDayFog,
+  sunny: WiDaySunny,
+  rain: WiRain
+}
+
+const renderState = state => {
+  const IconState = stateByName[state]
+
+  return <IconState />
+}
+
+const Weather = ({ temperature, state }) => {
   return (
     <div>
-      <WiCloud></WiCloud>
+      <IconContext.Provider value={{ size: '4em' }}>
+        {
+          renderState(state)
+        }
+      </IconContext.Provider>
       <Typography display='inline' variant='h2'>{temperature}</Typography>
     </div>
   )
 }
 
 Weather.propTypes = {
-  temperature: PropTypes.number.isRequired
+  temperature: PropTypes.number.isRequired,
+  state: PropTypes.string.isRequired
 }
 
 export default Weather
--- src/components/Weather/Weather.test.jsx
+++ src/components/Weather/Weather.test.jsx
@@ -6,7 +6,7 @@ import '@testing-library/jest-dom/extend-expect'
 
 test('Weather render', async () => {
   // AAA Arrange Act Assert
-  const { findByRole } = render(<Weather temperature={10} />)
+  const { findByRole } = render(<Weather temperature={10} state='sunny' />)
 
   // eslint-disable-next-line testing-library/prefer-screen-queries
   const temp = await findByRole('heading')
