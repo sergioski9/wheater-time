55dc84c7ee3852515f1cc4ca8437484a02c17ebc
sergio rodas <rodascruzsergio@gmail.com>
2022-06-06 23:25:01 -0400

    modify weather test and components

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,34 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Typography from '@material-ui/core/Typography'
-import { WiCloud, WiDayCloudy,
-  WiDayFog,
-  WiDaySunny,
-  WiRain } from 'react-icons/wi'
+import IconState, { validValues } from './../IconState'
 import { IconContext } from 'react-icons'
-
-
-const stateByName = {
-  cloud: WiCloud,
-  cloudy: WiDayCloudy,
-  fog: WiDayFog,
-  sunny: WiDaySunny,
-  rain: WiRain
-}
-
-const renderState = state => {
-  const IconState = stateByName[state]
-
-  return <IconState />
-}
 
 const Weather = ({ temperature, state }) => {
   return (
     <div>
       <IconContext.Provider value={{ size: '4em' }}>
-        {
-          renderState(state)
-        }
+        <IconState state={state} />
       </IconContext.Provider>
       <Typography display='inline' variant='h2'>{temperature}</Typography>
     </div>
@@ -37,7 +17,7 @@ const Weather = ({ temperature, state }) => {
 
 Weather.propTypes = {
   temperature: PropTypes.number.isRequired,
-  state: PropTypes.string.isRequired
+  state: PropTypes.oneOf(validValues).isRequired
 }
 
 export default Weather
