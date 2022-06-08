823bedb6a9e1f59bdea36f2a5b8a8e269b99a422
sergio rodas <rodascruzsergio@gmail.com>
2022-06-08 18:34:51 -0400

    added config and modify files in the src

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { BrowserRouter as Router,
+  Switch,
+  Route } from 'react-router-dom'
+import WelcomePage from './pages/WelcomePage'
+import MainPage from './pages/MainPage'
+import CityPage from './pages/CityPage'
+import NotFoundPage from './pages/NotFoundPage'
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <WelcomePage />
+        </Route>
+        <Route path='/main'>
+          <MainPage />
+        </Route>
+        <Route path='/city'>
+          <CityPage />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import App from './App'
+import { render } from '@testing-library/react'
+
+
+test('renders learn react link', () => {
+
+})
