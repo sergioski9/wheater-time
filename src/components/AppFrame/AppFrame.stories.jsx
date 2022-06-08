83fd57b10975a6e4f88997e68588725c7152d861
sergio rodas <rodascruzsergio@gmail.com>
2022-06-08 18:27:32 -0400

    added component appframe, must index,stories and components files

---
+++ src/components/AppFrame/AppFrame.stories.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppFrame from './AppFrame'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+export default {
+  title: 'AppFrame',
+  component: AppFrame
+}
+
+export const AppFrameExample = () => (
+    <Router>
+      <AppFrame />
+    </Router>
+)
