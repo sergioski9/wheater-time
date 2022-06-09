03391edde84911a5c9da87eb56879e4aed48b2db
sergio rodas <rodascruzsergio@gmail.com>
2022-06-09 08:22:16 -0400

    modify welcome screen

--- src/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -8,24 +8,18 @@ const WelcomeScreen = ({ children }) => {
   const myRefDiv = useRef(null)
   const [ vanta, setVanta ] = useState(0)
 
-  console.log('useRefDiv:', myRefDiv.current)
-
   useEffect(() => {
-    console.log('use effect:', myRefDiv.current)
 
     if (!vanta) {
       setVanta(Clouds({
         THREE,
         el: myRefDiv.current
       }))
-
-      console.log('vanta ya no es mas 0, ahora es 1')
     }
 
     return () => {
       if (vanta) {
         vanta.destroy()
-        console.log('Libero los espacios')
       }
     }
   }, [vanta])
