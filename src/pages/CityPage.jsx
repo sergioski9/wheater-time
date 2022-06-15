8f46d6773e0b47f58b816a225441b2ab4a2f61b1
sergio rodas <rodascruzsergio@gmail.com>
2022-06-14 21:32:24 -0400

    modify and refactored the cityPage component

--- src/pages/CityPage.jsx
+++ src/pages/CityPage.jsx
@@ -1,42 +1,21 @@
 import React from 'react'
-import AppFrame from './../components/AppFrame'
 import Grid from '@material-ui/core/Grid'
+import AppFrame from './../components/AppFrame'
 import CityInfo from './../components/CityInfo'
 import Weather from './../components/Weather'
 import WeatherDetails from './../components/WeatherDetails'
 import ForecastChart from '../components/ForecastChart'
 import Forecast from './../components/Forecast'
-
-
-const dataExample = [
-  { dayHour: 'vie 14', min: 19, max: 23 },
-  { dayHour: 'sab 15', min: 14, max: 19 },
-  { dayHour: 'dom 16', min: 25, max: 31 },
-  { dayHour: 'lun 17', min: 4, max: 10 },
-  { dayHour: 'mar 18', min: 9, max: 14 },
-  { dayHour: 'mie 19', min: 17, max: 26 },
-  { dayHour: 'jue 20', min: 21, max: 24 }
-]
-
-const forecastItemListExample = [
-  { hour: 18, state: 'clear', temperature: 17, weekDay: "Viernes" },
-  { hour: 6, state: 'clouds', temperature: 18, weekDay: "Sabado" },
-  { hour: 12, state: 'clouds', temperature: 20, weekDay: "Domingo" },
-  { hour: 18, state: 'rain', temperature: 19, weekDay: "Lunes" },
-  { hour: 6, state: 'clear', temperature: 14, weekDay: "Martes" },
-  { hour: 12, state: 'snow', temperature: 12, weekDay: "Miercoles" }
-]
+import useCityPage from './../hooks/useCityPage'
 
 const CityPage = () => {
+  const { chartData, city, forecastItemList } = useCityPage()
 
-  const city = 'Santa Cruz'
   const country = 'Bolivia'
   const state = 'clouds'
   const temperature = 20
   const humidity = 65
   const wind = 24
-  const data = dataExample
-  const forecastItemList = forecastItemListExample
 
   return (
     <AppFrame>
@@ -57,10 +36,14 @@ const CityPage = () => {
             <WeatherDetails humidity={humidity} wind={wind} />
         </Grid>
         <Grid item>
-          <ForecastChart data={data} />
+          {
+            chartData && <ForecastChart data={chartData} />
+          }
         </Grid>
         <Grid item>
-          <Forecast forecastItemList={forecastItemList} />
+          {
+            forecastItemList && <Forecast forecastItemList={forecastItemList} />
+          }
         </Grid>
       </Grid>
     </AppFrame>
