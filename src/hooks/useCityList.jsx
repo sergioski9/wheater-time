5e1d16230a98c55f18271932a564b0c4d0d7e013
sergio rodas <rodascruzsergio@gmail.com>
2022-06-15 21:00:45 -0400

    modify hook usecitylist for calling not repeat

--- src/hooks/useCityList.jsx
+++ src/hooks/useCityList.jsx
@@ -1,13 +1,10 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { validValues } from './../components/IconState'
-import { getCityCode } from './../utils/utils'
 import { getWeatherUrl } from './../utils/urls'
-import { toCelsius } from './../utils/utils'
+import getAllWeather from './../utils/transform/getAllWeather'
+import { getCityCode } from './../utils/utils'
 
-
-const useCityList = (cities) => {
-  const [allWeather, setAllWeather] = useState({})
+const useCityList = (cities, allWeather, onSetAllWeather) => {
   const [error, setError] = useState(null)
 
   useEffect(() => {
@@ -15,18 +12,16 @@ const useCityList = (cities) => {
       const url = getWeatherUrl({ city, countryCode })
 
       try {
+        const propName = getCityCode(city, countryCode)
+
+        onSetAllWeather({ [propName]: {} })
+
         const response = await axios.get(url)
 
-        const { data } = response
-        const temperature = toCelsius(data.main.temp)
-        const stateFromServer = data.weather[0].main.toLowerCase()
+        const allWeatherAux = getAllWeather(response, city, countryCode)
 
-        const state = validValues.includes(stateFromServer) ? stateFromServer : null
-
-        const propName = getCityCode(city, countryCode)
-        const propValue = { temperature, state }
-
-        setAllWeather(allWeather => ({ ...allWeather, [propName]: propValue }))
+        // setAllWeather(allWeather => ({ ...allWeather, ...allWeatherAux }))
+        onSetAllWeather({ ...allWeatherAux })
       } catch (error) {
         if (error.response) {
           setError('Ha ocurrido un error con el servidor del clima')
@@ -38,11 +33,15 @@ const useCityList = (cities) => {
       }
     }
 
-    cities.forEach(({ city, countryCode }) => setWeatherCity(city, countryCode))
+    cities.forEach(({ city, countryCode }) => {
+      if (!allWeather[getCityCode(city, countryCode)]) {
+        setWeatherCity(city, countryCode)
+      }
+    })
 
-  }, [cities])
+  }, [cities, onSetAllWeather, allWeather])
 
-  return { allWeather, error, setError }
+  return { error, setError }
 }
 
 export default useCityList
